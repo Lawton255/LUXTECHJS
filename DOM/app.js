aaac87e9f37c3dfa172add30fbbc144b36de57c8
Lawton255 <elawton255@gmail.com>
2020-08-03 00:07:26 +0300

    New DOM Assignment commit

---
+++ DOM/app.js
@@ -0,0 +1,6 @@
+var button = document.getElementById('alert-button');
+button.onclick = function(){
+    alert(
+"Welcome to JavaScript Training BootCamp, Lux Tech Academy is a free online training BootCamp which have crash and advance coding classes."     
+    );
+}
